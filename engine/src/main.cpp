b71fc54320e1885e8f9a29be589feeab5ce844d4
Jason Scherer <schereja@gmail.com>
2026-03-19 21:02:49 -0700

    Milestone 2: OpenGL 3.3 added with GLEW

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <GL/glew.h>
 #include <iostream>
 
 int main(int argc, char* argv[]){
@@ -7,12 +8,20 @@ int main(int argc, char* argv[]){
         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return -1;
     }
+
+    // Tell SDL we want OpenGL 3.3 Core profile
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    // Create the window with the OpenGL flag
     SDL_Window* window = SDL_CreateWindow(
         "Forge Engine",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         1280, 720,
-        SDL_WINDOW_SHOWN
+        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
     );
 
     if (!window) {
@@ -20,6 +29,36 @@ int main(int argc, char* argv[]){
         SDL_Quit();
         return -1;
     }
+
+    // Create OpenGL context
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!glContext) {
+        std::cerr << "SDL_GL_CreateContext Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Initialize GLEW
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "GLEW Error: " << glewGetErrorString(glewInit()) << std::endl;
+        SDL_GL_DeleteContext(glContext);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Print OpenGL and GLEW versions
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLEW Version: " << glewGetString(GLEW_VERSION) << std::endl;
+    
+    // Enable V-Sync
+    SDL_GL_SetSwapInterval(1);
+
+    //Set the clear color to dark gray
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+
 
     // Main Loop
     bool running = true;
@@ -37,10 +76,11 @@ int main(int argc, char* argv[]){
             }
         }
         // clear the screen to dark gray
-        SDL_Surface* surface = SDL_GetWindowSurface(window);
-        SDL_FillRect(surface, nullptr, SDL_MapRGB(surface->format, 50, 50, 50));
-        SDL_UpdateWindowSurface(window);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // Swap the buffers to display the current frame
+        SDL_GL_SwapWindow(window);
     }
+    SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
